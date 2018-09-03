a10fd17222a411fe8ba939b643f4bd4f045a9e5a
vidhigithub <vdchandnani15@gmail.com>
2018-09-03 14:04:16 +0530

    servlet

---
+++ vidhi1/src/java/next.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author divyesh
+ */
+@WebServlet(urlPatterns = {"/next"})
+public class next extends HttpServlet {
+
+     public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
+  
+   {
+       res.setContentType("text/html");
+       PrintWriter out=res.getWriter();
+       out.println("get method");
+       
+    }
+
+        
+         public void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
+        {
+            
+         res.setContentType("text/html");
+       PrintWriter out=res.getWriter();
+       out.println("post method");
+        }
+    
+}
